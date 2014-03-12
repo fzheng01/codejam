23bddf7801a5ae4bb1513f72e51098ac6c9c9d58
Feng Zheng <feng.zheng@freecause.com>
2014-03-11 23:43:01 -0400

    for test

---
+++ temporary/exceptions/ClassInheritance.java
@@ -0,0 +1,30 @@
+interface Work {
+    void logHours(int hours);
+}
+
+class Job {
+    void doOne() {
+        System.out.println("One");
+    }
+}
+
+class Programming extends Job implements Work{
+    // must public, as interface is implicitly public abstract
+    public void logHours(int hours) {
+        System.out.println("Programming logs " + hours + " hours");
+    }
+    void doTwo() {
+        System.out.println("Two");
+    }
+}
+
+public class ClassInheritance {
+    public static void main(String[] args) {
+        Job a = new Job();
+        Programming b = new Programming();
+        Job c = new Programming();
+        Work d = new Programming();
+        ((Programming) c).doTwo();
+        d.logHours(3);
+    }
+}
