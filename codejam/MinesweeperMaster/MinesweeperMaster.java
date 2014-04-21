bfc291bdbc2d08b5150ca7a178d94154f27d6fdb
Feng Zheng <feng.zheng@freecause.com>
2014-04-20 23:53:16 -0400

    solve the minesweeper

--- codejam/MinesweeperMaster/MinesweeperMaster.java
+++ codejam/MinesweeperMaster/MinesweeperMaster.java
@@ -1,106 +1,93 @@
 import java.util.*;
 import java.io.*;
 
-// have issue on 3rd scenario
+/**
+ * start from top left
+ * if it is single free cell, click it
+ * if it is single row or column, click top left
+ * 
+ * minimum solvable cell is 2x2, so all of its expansions are solvable
+ * example:
+ * c....**
+ * .....**
+ * ..???**
+ * ..???**
+ * ..???**
+ * *******
+ * 
+ * no mine in '.' cells, otherwise, unsovlable
+ * '?' cells are optional
+ * 
+ * find the rmax and cmax, so that free cells amount N satisfies the inequation
+ * 2*(rmax + cmax) <= N <= (rmax+1) * (cmax+1)
+ * 
+ */
 public class MinesweeperMaster {
-    private final int[][] board;
-    private boolean solvable = true;
+    private final byte[][] board;
+    private boolean solvable;
     private final int row;
     private final int col;
+    private final int safe;
     public MinesweeperMaster(int r, int c, int m) {
         row = r;
         col = c;
-        board = new int[r][c];
-        int mines = m, i = 0, j = 0;
-        boolean rt = true, lf = false, up = false, dn = false;
-        while (mines-- > 0) {
-            if (rt) {
-                board[i][j++] = -1;
-                if (j >= col || board[i][j] < 0) {
-                    rt = false;
-                    dn = true;
-                    i++;
-                    j--;
-                }
-            } else if (lf) {
-                board[i][j--] = -1;
-                if (j < 0 || board[i][j] < 0) {
-                    lf = false;
-                    up = true;
-                    i--;
-                    j++;
-                }
-            } else if (up) {
-                board[i--][j] = -1;
-                if (i < 0 || board[i][j] < 0) {
-                    up = false;
-                    rt = true;
-                    j++;
-                    i++;
-                }
-            } else {
-                board[i++][j] = -1;
-                if (i >= row || board[i][j] < 0) {
-                    dn = false;
-                    lf = true;
-                    j--;
-                    i--;
+        safe = row*col - m;
+        board = new byte[row][col];
+        board[0][0] = 1;
+        int counter = safe;
+        if (safe == 1) solvable = true;
+        else if (row == 1 || col == 1) {
+            solvable = true;
+            LOOP1:
+            for (int i = 0; i < row; i++) {
+                for (int j = 0; j < col; j++) {
+                    board[i][j] = 1;
+                    if (--counter <= 0) break LOOP1;
                 }
             }
         }
-        for (i = 0; i < row; i++) {
-            for (j = 0; j < col; j++) {
-                if (board[i][j] < 0) continue;
-                else board[i][j] = assignType(i, j);
-            }
-        }
-        for (i = 0; i < row; i++) {
-            for (j = 0; j < col; j++) {
-                if (board[i][j] == 1 && !evaluate(i, j)) {
-                    solvable = false;
-                    break;
+        else if (safe == 3 || safe == 5) solvable = false;
+        else {
+            solvable = false;
+            LOOP3:
+            for (int rmax = 1; rmax < row; rmax++) {
+                for (int cmax = 1; cmax < col; cmax++) {
+                    if (safe >= 2*(rmax+cmax) && safe <= (rmax+1)*(cmax+1)) {
+                        solvable = true;
+                        counter = safe - 2*(rmax+cmax);
+                        int i, j;
+                        for (i = 0; i <= 1; i++) {
+                            for (j = 0; j <= cmax; j++) {
+                                board[i][j] = 1;
+                            }
+                        }
+                        for (i = 0; i <= rmax; i++) {
+                            for (j = 0; j <= 1; j++) {
+                                board[i][j] = 1;
+                            }
+                        }
+                        LOOP2:
+                        for (i = 2; i <= rmax; i++) {
+                            for (j = 2; j <= cmax; j++) {
+                                if (counter-- <= 0) break LOOP2;
+                                board[i][j] = 1;
+                            }
+                        }
+                        break LOOP3;
+                    }
                 }
             }
         }
-    }
-    
-    private int assignType(int x, int y) {
-        for (int i = -1; i <= 1; i++) {
-            for (int j = -1; j <= 1; j++) {
-                if (i == 0 && j == 0) continue;
-                int xx = x + i, yy = y + j;
-                if (xx < 0 || xx >= row) continue;
-                if (yy < 0 || yy >= col) continue;
-                if (board[xx][yy] < 0) return 1;
-            }
-        }
-        return 0;
-    }
-    
-    private boolean evaluate(int x, int y) {
-        for (int i = -1; i <= 1; i++) {
-            for (int j = -1; j <= 1; j++) {
-                if (i == 0 && j == 0) continue;
-                int xx = x + i, yy = y + j;
-                if (xx < 0 || xx >= row) continue;
-                if (yy < 0 || yy >= col) continue;
-                if (board[xx][yy] == 0) return true;
-            }
-        }
-        return false;
     }
     
     public String toString() {
-//        if (!solvable) return "Impossible";
-        boolean didClick = false;
+        if (!solvable) return "Impossible";
         StringBuilder str = new StringBuilder();
         for (int i = 0; i < row; i++) {
             for (int j = 0; j < col; j++) {
-                if (board[i][j] < 0) str.append("*");
-                else if (board[i][j] == 0 && !didClick) {
-                    str.append("c");
-                    didClick = true;
-                } else str.append(".");
+                if (i == 0 && j == 0) str.append("c");
+                else if (board[i][j] > 0) str.append(".");
+                else str.append("*");
             }
             if (i < row-1) str.append("\n");
         }
@@ -108,10 +95,10 @@ public class MinesweeperMaster {
     }
     
     public static void main(String[] args) throws Exception {
-        if (args.length != 2)
-            throw new IllegalArgumentException("Require input and output filenames");
+        if (args.length == 0)
+            throw new IllegalArgumentException("Require input file name");
         Scanner sc = new Scanner(new FileReader(args[0]));
-        PrintWriter pw = new PrintWriter(new FileWriter(args[1]));
+        PrintWriter pw = new PrintWriter(new FileWriter(args[0].replaceFirst("[.][^.]+$", "").concat(".out")));
         int caseCnt = sc.nextInt();
         int dim = 4;
         for (int caseNum = 0; caseNum < caseCnt; caseNum++) {
