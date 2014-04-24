fe4a3c4a618fc34b8fe175cec6d10cecf5810689
Feng Zheng <feng.zheng@freecause.com>
2014-04-23 21:34:16 -0400

    trying

--- codejam/Bullseye/Bullseye.java
+++ codejam/Bullseye/Bullseye.java
@@ -11,12 +11,18 @@ public class Bullseye {
         int caseCnt = sc.nextInt();
         for (int caseNum = 0; caseNum < caseCnt; caseNum++) {
             pw.print("Case #" + (caseNum + 1) + ": ");
-            double r = sc.nextDouble();
-            double t = sc.nextDouble();
-            double result = 0.5*(Math.sqrt((r-0.5)*(r-0.5) + 2.0*t)-(r-0.5));
-            long n = (long) Math.floor(result);
-            if (t < 2.0*n*n+(2.0*r-1)*n) n--;
-            pw.println(n);
+            // double r = sc.nextDouble();
+            // double t = sc.nextDouble();
+            // double result = 0.5*(Math.sqrt((r-0.5)*(r-0.5) + 2.0*t)-(r-0.5));
+            // long n = (long) Math.floor(result);
+            // if (t < 2.0*n*n+(2.0*r-1)*n) n--;
+            long r = sc.nextLong();
+            long t = sc.nextLong();
+            long n = 0;
+            do {
+                t -= 4*(++n)+2*r-3;
+            } while (t > 0);
+            pw.println(--n);
         }
         pw.flush();
         pw.close();
