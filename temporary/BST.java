59acd7b8cd4827023f34258bf3598b0bb49370da
Feng Zheng <summitzf@gmail.com>
2014-07-20 12:32:24 -0400

    added bst practice

---
+++ temporary/BST.java
@@ -0,0 +1,44 @@
+public class BST {
+    private class Node {
+        int value;
+        Node left;
+        Node right;
+        int rank;
+        Node(int v) {
+            this.value = v;
+        }
+    }
+    private Node root;
+    public void add(int v) {
+        if (root == null) root = new Node(v);
+        else add(root, v);
+    }
+    private void add(Node n, int v) {
+        if (n.value >= v) {
+            if (n.left == null) n.left = new Node(v);
+            else add(n.left, v);
+        } else {
+            if (n.right == null) n.right = new Node(v);
+            else add(n.right, v);
+        }
+    }
+    public void sort() {
+        sort(root);
+        System.out.println();
+    }
+    private void sort(Node n) {
+        if (n != null) {
+            sort(n.left);
+            System.out.print(" " + n.value + " ");
+            sort(n.right);
+        }
+    }
+    public static void main(String[] args) {
+        int[] arr = {8, 3, 10, 6, 1, 4, 7, 14, 13};
+        BST bst = new BST();
+        for (int x : arr) {
+            bst.add(x);
+        }
+        bst.sort();
+    }
+}
