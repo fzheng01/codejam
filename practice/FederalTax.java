b1c59706e4ca560347af9549f54fd70a0351bf0a
fzheng <feng.zheng@freecause.com>
2014-09-08 12:24:17 -0400

    refactor the code

--- practice/FederalTax.java
+++ practice/FederalTax.java
@@ -1,58 +1,88 @@
+import java.lang.IllegalArgumentException;
+import java.util.Random;
+
 public class FederalTax {
     private float grossIncome;
     private float withheld;
+    private float[] bracket;
+    private float[] percentage;
+    private float standardDeductible;
+    private float personalExemption;
     private float deductible;
-    private float standardDeductible = 12400f;
-    private float childCredit = 1000f;
-    private float personalExemption = 3950f;
-    private int heads = 3;
-    
-    public FederalTax(float income, float withheld) {
+    private float childCredit;
+    private int headCount = 3;
+
+    public FederalTax(int year, float income, float withheld) {
         this.grossIncome = income;
         this.withheld = withheld;
-        this.deductible = standardDeductible + heads * personalExemption;
+        switch (year) {
+            case 2014:
+                this.bracket = new float[]{0f, 18150f, 73800f, 148850f, 226850f, 405100f, 457600f, Float.MAX_VALUE};
+                this.percentage = new float[]{0f, .1f, .15f, .25f, .28f, .33f, .35f, .396f};
+                this.standardDeductible = 12400f;
+                this.personalExemption = 3950f;
+                this.childCredit = 1000f;
+                break;
+            case 2013:
+                this.bracket = new float[]{0f, 17850f, 72500f, 146400f, 223050f, 398350f, 450000f, Float.MAX_VALUE};
+                this.percentage = new float[]{0f, .1f, .15f, .25f, .28f, .33f, .35f, .396f};
+                this.standardDeductible = 12200f;
+                this.personalExemption = 3900f;
+                this.childCredit = 1000f;
+                break;
+            default:
+                throw new IllegalArgumentException("invalid year for tax bracket");
+        }
+        this.deductible = standardDeductible + headCount * personalExemption;
     }
-    
+
+    private float getTax(float taxable) {
+        if (taxable <= 0f) throw new IllegalArgumentException("taxable amount must be positive");
+        float taxDue = 0f;
+        for (int i = 1; i < bracket.length; i++) {
+            if (this.bracket[i] < taxable) taxDue += this.percentage[i] * (this.bracket[i] - this.bracket[i - 1]);
+            else {
+                taxDue += this.percentage[i] * (taxable - this.bracket[i - 1]);
+                break;
+            }
+        }
+        return taxDue;
+    }
+
     public float addDeductible(float value) {
         this.deductible += value;
         return this.deductible;
     }
-    
+
     public float run() {
-        float tax = getTaxOfMarriedFilingJointly(grossIncome - deductible);
+        float tax = getTax(grossIncome - deductible);
         return withheld - (tax - childCredit);
     }
-    
-    private float getTaxOfMarriedFilingJointly(float taxable) {
-        if(taxable <= 18150) return 0.1f*taxable;
-        else if(taxable <= 73800) return 1815f + 0.15f*(taxable-18150);
-        else if(taxable <= 148850) return 10162.5f + 0.25f*(taxable-73800);
-        else if(taxable <= 226850) return 28925f + 0.28f*(taxable-148850);
-        else if(taxable <= 405100) return 50765f + 0.33f*(taxable-226850);
-        else if(taxable <= 457600) return 109587.5f + 0.35f*(taxable-405100);
-        else return 127962.5f + 0.396f*(taxable-457600);
-    }
-    
-    private float getTaxOfMarriedFilingJointly2013(float taxable) {
-        // for tax year 2013
-        if(taxable <= 17850) return 0.1f*taxable;
-        else if(taxable <= 72500) return 1785f + 0.15f*(taxable-17850);
-        else if(taxable <= 146400) return 10875f + 0.25f*(taxable-72500);
-        else if(taxable <= 223050) return 28925f + 0.28f*(taxable-146400); // inaccurate
-        else if(taxable <= 398350) return 50765f + 0.33f*(taxable-223050); // inaccurate
-        else if(taxable <= 450000) return 109587.5f + 0.35f*(taxable-398350); // inaccurate
-        else return 127962.5f + 0.396f*(taxable-450000); // inaccurate
-    }
-    
+
     public static void main(String[] args) {
-        float income = 100366.74f;
-        float withheld = 18411.01f;
-//        float income = 91571f;
-//        float withheld = 16226.56f;
-        FederalTax feng = new FederalTax(income, withheld);
-        feng.addDeductible(5500); // IRA deductible
-        System.out.printf("Gross Income: %.2f\n", feng.grossIncome);
-        System.out.printf("Taxable Income: %.2f\n", feng.grossIncome-feng.deductible);
-        System.out.printf("Tax Return: %.2f\n", feng.run());
+        // =========== 2013 ===========
+        float income = 91571f;
+        float withheld = 16226.56f;
+        FederalTax taxPayer = new FederalTax(2013, income, withheld);
+        taxPayer.addDeductible(5500); // IRA deductible
+        System.out.printf("2013 Gross Income: %.2f, Taxable Income: %.2f, Tax Return: %.2f\n", income, taxPayer.grossIncome - taxPayer.deductible, taxPayer.run());
+        // =========== 2014 ===========
+        income = 77404.18f - (10274.49f - 2946.81f);
+        withheld = 13065.33f;
+        // mid sep payment of fc
+        income += 4174.13f - 83.59f - 10.58f - 1.57f - 41.66f - 250.45f;
+        withheld += 668.21f;
+        // last payment of fc
+        income += .94f * 8 * 6.17f * 48.161186f;
+        withheld += 500f;
+        // ws payment till end of year
+        float ws401k = 0.08f;
+        income += 7 * (4916.67f - 110.37f - 31.95f - 35f - ws401k * 4916.67f);
+        withheld += 7 * 700f;
+//        income = 100366.74f;
+//        withheld = 18411.01f;
+        taxPayer = new FederalTax(2014, income, withheld);
+        taxPayer.addDeductible(5500); // IRA deductible
+        System.out.printf("2014 Gross Income: %.2f, Taxable Income: %.2f, Tax Return: %.2f\n", income, taxPayer.grossIncome - taxPayer.deductible, taxPayer.run());
     }
 }
