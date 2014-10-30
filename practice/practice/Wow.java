9297ce0f209a9dfb1369cf1d67917d186b39274a
Feng <fzheng@wordstream.com>
2014-10-30 10:41:16 -0400

    added practice

--- practice/practice/Wow.java
+++ practice/practice/Wow.java
@@ -1,4 +1,30 @@
 public class Wow {
+	private static int[] aux;
+	public static void sort(int[] a) {
+		int N = a.length;
+		if (N <= 1) return;
+		aux = new int[N];
+		sort(a, 0, N-1);
+	}
+	private static void sort(int[] a, int lo, int hi) {
+		if (hi <= lo) return;
+		int mid = lo + (hi-lo)/2;
+		sort(a, lo, mid);
+		sort(a, mid+1, hi);
+		merge(a, lo, mid, hi);
+	}
+	private static void merge(int[] a, int lo, int mid, int hi) {
+		for (int k = lo; k <= hi; k++) {
+			aux[k] = a[k];
+		}
+		int i = lo, j = mid+1;
+		for (int k = lo; k <= hi; k++) {
+			if (i > mid) a[k] = aux[j++];
+			else if (j > hi) a[k] = aux[i++];
+			else if (aux[i] <= aux[j]) a[k] = aux[i++];
+			else a[k] = aux[j++];
+		}
+	}
     public static void main(String[] main) {
         StringBuilder sb = new StringBuilder();
         String h1 = "HelloWorld";
