813529b6ff99dcbcda5b80f14ae89c1f4e1a940a
Feng Zheng <summitzf@gmail.com>
2015-04-12 20:20:06 -0400

    using new codejam template

--- codejam/2015Q/StandingOvation/StandingOvation.java
+++ codejam/2015Q/StandingOvation/StandingOvation.java
@@ -2,32 +2,35 @@ import java.util.*;
 import java.io.*;
 
 public class StandingOvation {
+    private void solution(Scanner sc, PrintWriter pw) {
+        int maxShy = sc.nextInt();
+        char[] audiences = sc.next().toCharArray();
+        int stands = 0, ans = 0;
+        for (int thres = 0; thres <= maxShy; thres++) {
+            int sit = audiences[thres] - '0';
+            if (sit > 0) {
+                if (stands < thres) {
+                    int delta = thres - stands;
+                    ans += delta;
+                    stands += delta;
+                }
+                stands += sit;
+            }
+        }
+        pw.println(ans);
+    }
+
     public static void main(String[] args) throws Exception {
-        if (args.length == 0)
+        if (args.length == 0) {
             throw new IllegalArgumentException("Require input file name");
+        }
         Scanner sc = new Scanner(new FileReader(args[0]));
         String outFilename = args[0].replaceFirst("[.][^.]+$", "").concat(".out");
         PrintWriter pw = new PrintWriter(new FileWriter(outFilename));
-        int caseCnt = Integer.parseInt(sc.nextLine());
+        int caseCnt = sc.nextInt();
         for (int caseNum = 0; caseNum < caseCnt; caseNum++) {
             pw.print("Case #" + (caseNum + 1) + ": ");
-            String[] testCase = sc.nextLine().split("\\s+");
-            int maxShy = Integer.parseInt(testCase[0]);
-            String audiences = testCase[1];
-            int stand = 0;
-            int addition = 0;
-            for (int thres = 0; thres <= maxShy; thres++) {
-                int sit = audiences.charAt(thres) - '0';
-                if (sit > 0) {
-                    if (stand < thres) {
-                        int delta = thres - stand;
-                        addition += delta;
-                        stand += delta;
-                    }
-                    stand += sit;
-                }
-            }
-            pw.println(addition);
+            new StandingOvation().solution(sc, pw);
         }
         pw.flush();
         pw.close();
